57f8b2ce94abe13c0d3626369893bd85fdfae6c1
tngud100 <33285109@naver.com>
2024-01-10 18:05:50 +0900

    deploy test

--- src/main/java/com/example/keyboard/service/BoardService.java
+++ src/main/java/com/example/keyboard/service/BoardService.java
@@ -1,5 +1,9 @@
 package com.example.keyboard.service;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BoardService {
-
+    public String findData(){
+        return "deploy complete";
+    }
 }
