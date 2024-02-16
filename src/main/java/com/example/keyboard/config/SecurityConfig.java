2dabbb15d26d248d273c0b166e2bd6bd9baafc77
tngud100 <33285109@naver.com>
2024-02-16 16:10:40 +0900

    security debug

--- src/main/java/com/example/keyboard/config/SecurityConfig.java
+++ src/main/java/com/example/keyboard/config/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
 
                         CorsConfiguration configuration = new CorsConfiguration();
 
-                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedOrigins(Collections.singletonList("http://3.34.152.132:3000"));
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowCredentials(true);
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
