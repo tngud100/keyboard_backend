a37a2c4fdd24d6ce819d2d40c54e93f7d8d2a727
tngud100 <33285109@naver.com>
2024-02-16 14:56:51 +0900

    security debug

--- src/main/java/com/example/keyboard/config/SecurityConfig.java
+++ src/main/java/com/example/keyboard/config/SecurityConfig.java
@@ -70,11 +70,13 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/path/to/public/api").permitAll()
-                        .requestMatchers("/product/**").hasAuthority("ROLE_ADMIN")
-                        .requestMatchers("/api/check").hasAuthority("ROLE_ADMIN")
-                        .requestMatchers("/api/logout", "/api/join/send", "/api/join/verify").permitAll()
-                        .anyRequest().authenticated());
+//                        .requestMatchers("/path/to/public/api").permitAll()
+//                        .requestMatchers("/product/**").hasAuthority("ROLE_ADMIN")
+//                        .requestMatchers("/api/check").hasAuthority("ROLE_ADMIN")
+//                        .requestMatchers("/api/logout", "/api/join/send", "/api/join/verify").permitAll()
+//                        .anyRequest().authenticated());
+                          .anyRequest().permitAll());
+
 
         http
                 .sessionManagement((session) -> session
